e45d47ff9bf06f110b7cdebc97a1cec496a5bf28
SilasDemaryJr <silunu94@kingshigh.school.nz>
2025-05-30 14:56:03 +1200

    Added script to main menu buttons

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_settings_pressed() -> void:
+	print("settings")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
