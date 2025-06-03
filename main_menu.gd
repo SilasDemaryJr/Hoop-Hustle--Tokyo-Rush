04e3220eb731a149f22d4f7f30edd71822314c0d
SilasDemaryJr <silunu94@kingshigh.school.nz>
2025-06-04 09:56:05 +1200

    Added volume sliders to option button

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,28 @@
 extends Control
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 
-func _on_start_pressed() -> void:
+func _process(_delta):
+	pass
+
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
+	
+	
+func _on_start_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 
-func _on_settings_pressed() -> void:
+func _on_settings_pressed():
 	print("settings")
+	main_buttons.visible = false
+	options.visible = true
 
-
-func _on_exit_pressed() -> void:
+func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
